2c6c03d4c811c01cd21f322d49165fa5a1bf2678
manikonda9 <kondaiah121@gmail.com>
2019-01-24 22:03:50 +0530

    adding table

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import img from "./contents/icons/add.svg"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>MMK Application</h1>
-          <h3>React Application</h3>
-        </header>
-      </div>
+      <section>
+        <div>Items (200)<span className="sidebar"></span></div>
+        <div>2 Selected</div>
+        <div>Do Something</div>
+        <div><img src={require("./contents/icons/add.svg")} alt="add" /></div>
+        <div><img src={require("./contents/icons/add.svg")} alt="add" /></div>
+        <div><img src={require("./contents/icons/add.svg")} alt="add" /></div>
+        <div><img src={require("./contents/icons/add.svg")} alt="add" /><span className="sidebar"></span></div>
+        <div><img src={require("./contents/icons/add.svg")} alt="add" /></div>
+        <div><input type="text" placeholder="Find" /><span className="sidebar"></span></div>
+        <div><img src={require("./contents/icons/add.svg")} alt="add" /></div>
+
+        <table className="table responsive">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Name</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+            </tr>
+            <tr>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+            </tr>
+            <tr>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+            </tr>
+            <tr>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+            </tr>
+            <tr>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+              <td>Manikonda</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     );
   }
 }
